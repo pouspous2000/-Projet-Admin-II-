009fb7ca4f6f02c9f5fc2cfb4b28d617eb6af1e1
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-26 10:42:46 +0200

    connection a mysql

--- server-web/site-b2b/php/index.php
+++ server-web/site-b2b/php/index.php
@@ -1,3 +1,12 @@
 <?php
-echo 'Welcome on woodytoys b2b';
+$host= 'db';
+$user="b2b";
+$password="woody";
+$db ="db_woody";
+
+$conn = new mysqli($host, $user, $password, $db);
+if ($conn -> connect_error){
+    echo 'connection failed' . $conn->connect_error;
+}
+echo 'Connecté à MSQL avec succès!';
 ?>
